9401df21c3d9a12a726c73d87ef7ee4997deda61
juliesantiago <santiago.sjulie@gmail.com>
2023-04-28 17:26:56 -0300

    feat: :construction: Create AlunoController and migration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/olá', function(){
-    echo('olá mundo');   
-});
+Route::get('/olá', [HomeController::class, 'index']); 
+
